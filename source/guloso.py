b6b34f0ffc6dd84444eaa934f4648ce22efa111d
Flavia <flavianunes@decom.ufop.br>
2018-10-18 23:16:28 -0300

    Correção bug guloso

--- source/guloso.py
+++ source/guloso.py
@@ -5,13 +5,13 @@
 def selecao(atividades, n):
     compativeis = []
     atividades.sort(key=lambda x: x[1])
-    i = 1
+    i = 0
     compativeis.append(atividades[0])
-    for m in range (2, n):
+    for m in range (1, n):
         s1, f1 = atividades[m]
         s2, f2 = atividades[i]
         if s1 >= f2:
             compativeis.append(atividades[m])
     print compativeis
        
-selecao([(1,2),(3,4), (1,1)], 3)
+#selecao([(1,2),(3,4), (1,1)], 3)
