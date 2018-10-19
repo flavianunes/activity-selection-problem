cd3a2a2528e4a958540f3aa3c9d211de194e1a2c
Maycon Amaro <maycon.amaro97@gmail.com>
2018-10-18 22:25:15 -0300

    main

--- source/dinamico.py
+++ source/dinamico.py
@@ -1,1 +1,11 @@
+# coding: utf-8
+
+def selecao(atividades, n):
+    """
+    atividades é uma lista de tuplas. n é o tamanho da lista. Use
+    
+    inicio, fim = atividades[posicao]
+    
+    para colher os tempos de uma atividade
+    """
  
