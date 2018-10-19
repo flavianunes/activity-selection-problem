c1feabaeb602ad3190967d9065123535b9ea0d6d
Flavia <flavianunes@decom.ufop.br>
2018-10-18 22:43:06 -0300

    Adicionando algoritmo guloso

--- source/guloso.py
+++ source/guloso.py
@@ -1,1 +1,15 @@
- 
+# coding: utf-8
+
+compativeis = []
+
+def selecao(atividades, n):
+    atividades.sort(key=lambda x: x[1])
+    i = 1
+    compativeis.append(atividades[0])
+    for m in range (2, n):
+        s1, f1 = atividades[m]
+        s2, f2 = atividades[i]
+        if s1 >= f2:
+            compativeis.append(atividades[m])
+    print compativeis
+       
