9865e1d6e341a712afcc2970a8170f0e549be191
Flavia <flavianunes@decom.ufop.br>
2018-10-21 12:48:57 -0300

    Arrumei o bug. Fixes #2

--- source/guloso.py
+++ source/guloso.py
@@ -12,6 +12,7 @@ def selecao(atividades, n):
         s2, f2 = atividades[i]
         if s1 >= f2:
             compativeis.append(atividades[m])
+            i = m
     print compativeis
        
 #selecao([(1,2),(3,4), (1,1)], 3)
