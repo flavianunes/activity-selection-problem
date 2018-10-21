e84f5167656a7fe7db25092360098f15c6212b41
Maycon Amaro <maycon.amaro97@gmail.com>
2018-10-21 18:03:43 -0300

    indices

--- source/guloso.py
+++ source/guloso.py
@@ -4,14 +4,14 @@
 def selecao(atividades, n):
     
     compativeis = []
-    atividades.sort(key=lambda x: x[1])
+    atividades.sort(key=lambda x: x[2])
     i = 0
-    compativeis.append(atividades[0])
+    compativeis.append(atividades[0][0])
     for m in range (1, n):
-        s1, f1 = atividades[m]
-        s2, f2 = atividades[i]
+        index1, s1, f1 = atividades[m]
+        index2, s2, f2 = atividades[i]
         if s1 >= f2:
-            compativeis.append(atividades[m])
+            compativeis.append(index1)
             i = m
     print compativeis
     
