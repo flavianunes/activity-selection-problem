4df88989ab34f5cc598d1e128d61623271e5b393
Maycon Amaro <maycon.amaro97@gmail.com>
2018-11-02 19:36:36 -0300

    backtracking

--- source/backtracking.py
+++ source/backtracking.py
@@ -1,10 +1,36 @@
 # coding: utf-8
+# backtracking.py
 
 def selecao(atividades, n):
-    """
-    atividades é uma lista de triplas. n é o tamanho da lista. Use
     
-    num_tarefa, inicio, fim = atividades[posicao]
+    compativeis = subproblema(atividades, [])
+    print([indice for indice, s1, f1 in compativeis])
     
-    para colher os tempos e o índice de uma atividade
-    """
+def subproblema(atividades, selecionadas):
+    
+    # Criando versão com atividade da lista de selecionadas
+    sel = selecionadas[:]
+    sel.append(atividades[0])
+    
+    # Se só houver uma atividade não é necessário recursão
+    if len(atividades) == 1:
+        return sel if isConjuntoValido(sel) else selecionadas
+    
+    # Teste com a atividade. Se a inclusão da atividade nos dá um conjunto inválido, descarta-se.
+    x = subproblema(atividades[1:], sel) if isConjuntoValido(sel) else selecionadas 
+    
+    # Teste sem a atividade
+    y = subproblema(atividades[1:], selecionadas)
+    
+    return x if len(x) >= len(y) else y
+    
+
+def isConjuntoValido(atividades):
+    n = len(atividades)
+    for i in range(n-1):
+        index1, s1, f1 = atividades[i]
+        for j in range(i+1, n):
+            index2, s2, f2 = atividades[j]
+            if s2 >= s1 and s2 < f1 or s1 >= s2 and s1 < f2:
+                return False
+    return True
